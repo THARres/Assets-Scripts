fc16efe0f36cf31de0b149a14c5adbf38f81ba7b
wmichaelv <wmichaelv@hotmail.com>
2014-05-26 10:19:26 -0700

    implement mainMenu

---
+++ Miscellaneous/scene/MainMenuScreen.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScreen : MonoBehaviour {
+
+	public int buttonWidth = 200;
+	public int buttonHeight = Screen.height / 8;
+	public int spacing = Screen.height / 8;
+
+	public string str;
+	
+	void Start() {
+		/* Set BackGround */
+		GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Touhou-background/gensokyonewmap");
+  }
+
+	void OnGUI () {
+
+		GUILayout.BeginArea(new Rect ((Screen.width - buttonWidth) / 2, 
+		                    Screen.height / 6, buttonWidth, 400));
+
+			if (GUILayout.Button("Start", GUILayout.Height(buttonHeight))) {
+				Application.LoadLevel("touhouMain");
+			}
+			GUILayout.Space(spacing);
+			if (GUILayout.Button("Continue", GUILayout.Height(buttonHeight))) {
+				// Implementation Pending
+			}
+
+			GUILayout.Space(spacing);
+			if (GUILayout.Button("End", GUILayout.Height(buttonHeight))) {
+				Application.Quit();
+			}
+		GUILayout.EndArea();
+	}
+}
