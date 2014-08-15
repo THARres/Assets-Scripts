456e053c82ce19e4d4473b53d22369abbf6315c4
wmichaelv <wmichaelv@hotmail.com>
2014-08-14 23:33:39 -0700

    physics component update

--- Character/common/CharaPhysicsManager.cs
+++ Character/common/CharaPhysicsManager.cs
@@ -1,22 +1,29 @@
 public class CharaPhysicsManager : UnityEngine.MonoBehaviour {
 
 	/* Everything is controlled by AI until the player jumps in */
-	public bool   AI = true;
+	public bool AIFlag = true;
+
+	/* Script Attached To GameObject */
+	public CharaAIManager        AI;
+	public CharaInputManager     Input;
+	public CharaParameterManager Parameter;
+
+	public int                   Direction;
+
+	private bool u,l,d,r;
 
 	/**************************************************************
 	 *          Action and Movement State Can Be Combined         *
-	 * Emotion State Will Replace Action and Movement when filled *
 	 **************************************************************/
 	public string ActionState = "";
 	public string MovementState = "";
-
-	public string EmotionState = "";
 	
 	public void Start() {
 		PreStart();
 		SetDefaultValue();
 		PostStart();
 	}
+
  /*
   ██▒▒▒▒██▒██▒██████▒▒████████▒██▒▒▒▒██▒▒█████▒▒██▒▒▒▒▒
   ██▒▒▒▒██▒██▒██▒▒▒██▒▒▒▒██▒▒▒▒██▒▒▒▒██▒██▒▒▒██▒██▒▒▒▒▒
@@ -25,8 +32,14 @@ public class CharaPhysicsManager : UnityEngine.MonoBehaviour {
   ▒▒████▒▒▒██▒██▒▒▒▒██▒▒▒██▒▒▒▒▒██████▒▒██▒▒▒██▒███████
  */
 
+	public virtual void ExecuteDash() {
+		/* Do Transform Movement Here */
+	}
 	public virtual void ExecuteFire() {
 		/* Do Particle System Here */
+	}
+	public virtual void ExecuteWalk() {
+		/* Do Transform Movement Here */
 	}
 
 	public virtual void PreStart() {}
@@ -39,28 +52,34 @@ public class CharaPhysicsManager : UnityEngine.MonoBehaviour {
   ▒██▒▒▒██▒██▒▒▒▒▒▒▒▒▒▒▒██▒▒▒
   ▒▒█████▒▒████████▒▒▒▒▒██▒▒▒
  */
-	public string GetPhysicsState() {
-		if (AI) {
+	private int GetDirectionFromAI() {
+		return System.Convert.ToInt32(AI.GetCommand("direction"));
+	}
+	private int GetDirectionFromInput() {
+		u = Input.GetKey("up0");
+		l = Input.GetKey("left0");
+		d = Input.GetKey("down0");
+		r = Input.GetKey("right0");
+		return u ? 
+			(d ? (l ? (r ? 0 : 4) : (r ? (l ? 0 : 6) : 0)) : (l ? (r ? 0 : 7) : (r ? (l ? 0 : 9) : 8))) : 
+			(l ? (r ? (d ? 2 : 0) : (d ? 1 : 4)) : (r ? (d ? 3 : 6) : (d ? 2 : 0)));
+	}
+	private string GetPhysicsState() {
+		if (AIFlag) {
 			return GetPhysicsStateFromAI();
 		} else {
 			return GetPhysicsStateFromInput();
 		}
 	}
-
 	private string GetPhysicsStateFromAI() {
-		return "Idle";
+		SetActionStateFromAI();
+		SetMovementStateFromAI();
+		return MovementState + ActionState;
 	}
-
 	private string GetPhysicsStateFromInput() {
-		if (GetComponent<CharaInputManager>().GetKey("fire")) {
-			ExecuteFire();
-			ActionState = "Fire";
-			MovementState = "Idle";
-		} else {
-			ActionState = "";
-			MovementState = "Idle";
-		}
-		return (EmotionState != "") ? EmotionState : MovementState + ActionState;
+		SetActionStateFromInput();
+		SetMovementStateFromInput();
+		return MovementState + ActionState;
 	}
 
  /*
@@ -70,5 +89,76 @@ public class CharaPhysicsManager : UnityEngine.MonoBehaviour {
   ▒▒▒▒▒▒██▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒
   ▒██████▒▒████████▒▒▒▒██▒▒▒
  */
-	public void SetDefaultValue() {}
+
+	private void SetActionStateFromAI() {
+		if ((bool)AI.GetCommand("fire")) {
+			ExecuteFire();
+			ActionState = "fire";
+		} else {
+			ActionState = "";
+		}
+	}
+	private void SetActionStateFromInput() {
+		if (Input.GetKey("fire")) {
+			ExecuteFire();
+			ActionState = "fire";
+		} else {
+			ActionState = "";
+		}
+	}
+	private void SetDefaultValue() {
+		AI        = GetComponent<CharaAIManager>();
+		Input     = GetComponent<CharaInputManager>();
+		Parameter = GetComponent<CharaParameterManager>();
+	}
+	private void SetMovementStateFromAI() {
+		Direction = GetDirectionFromAI();
+		switch (Direction) {
+		case 0:
+		default:
+			MovementState = "idle";
+			break;
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+			if ((bool)AI.GetCommand("dash")) {
+				ExecuteDash();
+				MovementState = "dash";
+			} else {
+				ExecuteWalk();
+				MovementState = "walk";
+			}
+			break;
+		}
+	}
+	private void SetMovementStateFromInput() {
+		Direction = GetDirectionFromInput();
+		switch (Direction) {
+		case 0:
+		default:
+			MovementState = "idle";
+			break;
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+			if (Input.GetKey("dash")) {
+				ExecuteDash();
+				MovementState = "dash";
+			} else {
+				ExecuteWalk();
+				MovementState = "walk";
+			}
+			break;
+		}
+	}
 }
