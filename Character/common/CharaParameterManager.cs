bfdb102bdbf7a4d824363f1e23948b9f0662840c
wmichaelv <wmichaelv@hotmail.com>
2014-08-14 23:32:46 -0700

    Simplify Parameter Manager

--- Character/common/CharaParameterManager.cs
+++ Character/common/CharaParameterManager.cs
@@ -1,10 +1,10 @@
 public class CharaParameterManager : UnityEngine.MonoBehaviour {
 
 	/* 0 is default input, please overload on PreStart() */
-	private float   charaHP     = 0; /* Store current hp values */
-	private float   charaMaxHP  = 0; /* Store max hp */
-	private float   charaBuffHP = 0; /* Store buffed hp values */
-	private float   charaMovSpd = 0; /* Store charaMovementSpeed */
+	public float   HP     = 0; /* Store current hp values */
+	public float   MaxHP  = 0; /* Store max hp */
+	public float   BuffHP = 0; /* Store buffed hp values */
+	public float   MovSpd = 0; /* Store charaMovementSpeed */
 
 	/* Stores All Parameter Here */
 	public System.Collections.Generic.Dictionary<string, float> Parameter = 
@@ -17,7 +17,7 @@ public class CharaParameterManager : UnityEngine.MonoBehaviour {
 	}
 	public void UpdateParameter(System.Collections.Generic.Dictionary<string, float> list) {
 		foreach (var param in list) {
-			Parameter[param.Key] = param.Value;
+			Set(param.Key, param.Value);
 		}
 	}
 
@@ -39,18 +39,9 @@ public class CharaParameterManager : UnityEngine.MonoBehaviour {
    ▒██▒▒▒██▒██▒▒▒▒▒▒▒▒▒▒▒██▒▒▒
    ▒▒█████▒▒████████▒▒▒▒▒██▒▒▒
   */
-  public float GetCharaHP() {
-		return charaHP;
-	}
-	public float GetCharaMaxHP() {
-		return charaMaxHP;
-	}
-	public float GetCharaBuffHP() {
-		return charaBuffHP;
-	}
-	public float GetCharaMovSpd() {
-		return charaMovSpd;
-	}
+  public float Get(string param) {
+  	return Parameter[param];
+  }
 
   /*
    ▒██████▒▒████████▒████████
@@ -60,27 +51,14 @@ public class CharaParameterManager : UnityEngine.MonoBehaviour {
    ▒██████▒▒████████▒▒▒▒██▒▒▒
   */
 
-  public float SetCharaHP(float num) {
-		charaHP = num;
-		return charaHP;
-	}
-	public float SetCharaMaxHP(float num) {
-		charaMaxHP = num;
-		return charaMaxHP;
-	}
-	public float SetCharaBuffHP(float num) {
-		charaBuffHP = num;
-		return charaBuffHP;
-	}
-	public float SetCharaMovSpd(float num) {
-		charaMovSpd = num;
-		return charaMovSpd;
+	public void Set(string param, float value) {
+		Parameter[param] = value;
 	}
 	public void SetDefaultValue() {
-		Parameter.Add("HP", charaHP);
-		Parameter.Add("MaxHP", charaMaxHP);
-		Parameter.Add("BuffHP", charaBuffHP);
-		Parameter.Add("MoveSpeed", charaMovSpd);
+		Parameter.Add("HP"        , HP);
+		Parameter.Add("MaxHP"     , MaxHP);
+		Parameter.Add("BuffHP"    , BuffHP);
+		Parameter.Add("MoveSpeed" , MovSpd);
 	}
 
 }
