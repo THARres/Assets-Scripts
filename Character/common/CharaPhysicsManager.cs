bee5949ca52f448f555719cdff3d06153ae00d7a
wmichaelv <wmichaelv@hotmail.com>
2014-08-15 19:58:26 -0700

    trim down physics manager due to compact input

--- Character/common/CharaPhysicsManager.cs
+++ Character/common/CharaPhysicsManager.cs
@@ -4,7 +4,7 @@ public class CharaPhysicsManager : UnityEngine.MonoBehaviour {
 	public bool AIFlag = true;
 
 	/* Script Attached To GameObject */
-	public CharaAIManager        AI;
+	public CharaInputManager     AI;
 	public CharaInputManager     Input;
 	public CharaParameterManager Parameter;
 
@@ -52,33 +52,23 @@ public class CharaPhysicsManager : UnityEngine.MonoBehaviour {
   ▒██▒▒▒██▒██▒▒▒▒▒▒▒▒▒▒▒██▒▒▒
   ▒▒█████▒▒████████▒▒▒▒▒██▒▒▒
  */
-	private int GetDirectionFromAI() {
-		return System.Convert.ToInt32(AI.GetCommand("direction"));
-	}
-	private int GetDirectionFromInput() {
-		u = Input.GetKey("up0");
-		l = Input.GetKey("left0");
-		d = Input.GetKey("down0");
-		r = Input.GetKey("right0");
+	private int GetDirection(CharaInputManager input) {
+		u = input.GetKey("up0");
+		l = input.GetKey("left0");
+		d = input.GetKey("down0");
+		r = input.GetKey("right0");
 		return u ? 
 			(d ? (l ? (r ? 0 : 4) : (r ? (l ? 0 : 6) : 0)) : (l ? (r ? 0 : 7) : (r ? (l ? 0 : 9) : 8))) : 
 			(l ? (r ? (d ? 2 : 0) : (d ? 1 : 4)) : (r ? (d ? 3 : 6) : (d ? 2 : 0)));
 	}
 	private string GetPhysicsState() {
 		if (AIFlag) {
-			return GetPhysicsStateFromAI();
+			SetActionState(AI);
+			SetMovementState(AI);
 		} else {
-			return GetPhysicsStateFromInput();
+			SetActionState(Input);
+			SetMovementState(Input);
 		}
-	}
-	private string GetPhysicsStateFromAI() {
-		SetActionStateFromAI();
-		SetMovementStateFromAI();
-		return MovementState + ActionState;
-	}
-	private string GetPhysicsStateFromInput() {
-		SetActionStateFromInput();
-		SetMovementStateFromInput();
 		return MovementState + ActionState;
 	}
 
@@ -90,16 +80,8 @@ public class CharaPhysicsManager : UnityEngine.MonoBehaviour {
   ▒██████▒▒████████▒▒▒▒██▒▒▒
  */
 
-	private void SetActionStateFromAI() {
-		if ((bool)AI.GetCommand("fire")) {
-			ExecuteFire();
-			ActionState = "fire";
-		} else {
-			ActionState = "";
-		}
-	}
-	private void SetActionStateFromInput() {
-		if (Input.GetKey("fire")) {
+	private void SetActionState(CharaInputManager input) {
+		if (input.GetKey("fire")) {
 			ExecuteFire();
 			ActionState = "fire";
 		} else {
@@ -107,37 +89,12 @@ public class CharaPhysicsManager : UnityEngine.MonoBehaviour {
 		}
 	}
 	private void SetDefaultValue() {
-		AI        = GetComponent<CharaAIManager>();
-		Input     = GetComponent<CharaInputManager>();
+		AI        = GetComponent<CharaAiInput>();
+		Input     = GetComponent<CharaPlayerInput>();
 		Parameter = GetComponent<CharaParameterManager>();
 	}
-	private void SetMovementStateFromAI() {
-		Direction = GetDirectionFromAI();
-		switch (Direction) {
-		case 0:
-		default:
-			MovementState = "idle";
-			break;
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-		case 6:
-		case 7:
-		case 8:
-		case 9:
-			if ((bool)AI.GetCommand("dash")) {
-				ExecuteDash();
-				MovementState = "dash";
-			} else {
-				ExecuteWalk();
-				MovementState = "walk";
-			}
-			break;
-		}
-	}
-	private void SetMovementStateFromInput() {
-		Direction = GetDirectionFromInput();
+	private void SetMovementState(CharaInputManager input) {
+		Direction = GetDirection(input);
 		switch (Direction) {
 		case 0:
 		default:
