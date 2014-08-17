40193cb206041ec59c8b6ed75824eecb01dc30da
wmichaelv <wmichaelv@hotmail.com>
2014-08-17 05:08:06 -0700

    Rework Map Script - Trim Down Design

---
+++ Map/Map.cs
@@ -0,0 +1,14 @@
+﻿public class Map : UnityEngine.MonoBehaviour {
+
+	public UnityEngine.GameObject Player = null;
+	public bool CameraHasUpdate = true;
+
+	public void Start () {}
+
+	public void UpdatePlayer(UnityEngine.GameObject player) {
+		if (Player != player) {
+			Player = player;
+			CameraHasUpdate = false;
+		}
+	}
+}
