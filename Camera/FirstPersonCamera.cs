be63ce33dfac55586083a0696d6251a3fbc94c3d
wmichaelv <wmichaelv@hotmail.com>
2014-08-17 05:09:00 -0700

    Finish Camera Script (Working Rough Design)

---
+++ Camera/FirstPersonCamera.cs
@@ -0,0 +1,51 @@
+﻿public class FirstPersonCamera : UnityEngine.MonoBehaviour {
+
+	public Map  Map     = null;
+	public bool Running = false;
+	private float h;
+	private UnityEngine.Vector3 pos = new UnityEngine.Vector3(0F,0F,0F);
+
+	public void StartFirstPersonCamera(Map map, UnityEngine.GameObject mama, float charaHeight) {
+		SetMap(map);
+		SetParent(mama);
+		SetTransform(charaHeight);
+		SetFieldOfView();
+	}
+
+	public void UpdateFirstPersonCamera() {
+		if (UnityEngine.Input.GetAxis("Mouse ScrollWheel") < 0) {
+			camera.fieldOfView = 2;
+			pos.x = 0F;
+			pos.y = h * 0.9F;
+			pos.z = h * 2F;
+			transform.localPosition = pos;
+			Running = false;
+		}
+	}
+
+ /*
+  ▒██████▒▒████████▒████████
+  ██▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒
+  ▒██████▒▒████████▒▒▒▒██▒▒▒
+  ▒▒▒▒▒▒██▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒
+  ▒██████▒▒████████▒▒▒▒██▒▒▒
+ */
+	private void SetMap(Map map) {
+		Map = map;
+	}
+	private void SetParent(UnityEngine.GameObject mama) {
+		transform.parent = mama.transform;
+	}
+	private void SetTransform(float charaHeight) {
+		h = charaHeight;
+		pos.x = 0F;
+		pos.y = h * 0.9F;
+		pos.z = -2F;
+		transform.localPosition = pos;
+		transform.localEulerAngles = new UnityEngine.Vector3(0F,180F,0F);
+	}
+	private void SetFieldOfView() {
+		camera.fieldOfView = 50;
+	}
+
+}
---
+++ Camera/ThirdPersonCamera.cs
@@ -0,0 +1,77 @@
+﻿public class ThirdPersonCamera : UnityEngine.MonoBehaviour {
+
+	public Map                    Map         = null;
+	public UnityEngine.GameObject Subject     = null;
+	public float                  FieldOfView = 50;
+
+	private float h;
+	private UnityEngine.Vector3 pos = new UnityEngine.Vector3(0F,0F,0F);
+	private FirstPersonCamera fpc;
+
+	public void Start() {
+		SetDefaultValue();
+	}
+
+	public void Update() {
+		if (!Map.CameraHasUpdate) {
+			Subject = Map.Player;
+			UpdateCamera();
+			Map.CameraHasUpdate = true;
+		}
+		UpdateCameraFieldOfView();
+	}
+
+	private void UpdateCamera() {
+		UpdateCameraParent();
+		ResetCameraFieldOfView();
+		UpdateCameraTransform();
+	}
+
+	private void UpdateCameraParent() {
+		transform.parent = Subject.transform;
+	}
+
+	private void UpdateCameraFieldOfView() {
+		if (!fpc.Running) {
+			if (camera.fieldOfView >= 2F && camera.fieldOfView <= 98F) {
+				camera.fieldOfView += UnityEngine.Input.GetAxis("Mouse ScrollWheel") * -10F;
+			} else {
+				if (camera.fieldOfView < 2F) {
+					camera.fieldOfView = 2F;
+					fpc.StartFirstPersonCamera(Map, Subject, h);
+					fpc.Running = true;
+				} else {
+					camera.fieldOfView = 98F;
+				}
+			}
+		} else {
+			fpc.UpdateFirstPersonCamera();
+		}
+	}
+
+	private void UpdateCameraTransform() {
+		h = Subject.transform.FindChild("Mesh").gameObject.transform.FindChild("Mesh0").gameObject.GetComponent<UnityEngine.SkinnedMeshRenderer>().bounds.size.y;
+		pos.x = 0F;
+		pos.y = h * 0.9F;
+		pos.z = h * 2F;
+		transform.localPosition = pos;
+		transform.localEulerAngles = new UnityEngine.Vector3(0F,180F,0F);
+	}
+
+	private void ResetCameraFieldOfView() {
+		camera.fieldOfView = FieldOfView;
+	}
+
+ /*
+  ▒██████▒▒████████▒████████
+  ██▒▒▒▒▒▒▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒
+  ▒██████▒▒████████▒▒▒▒██▒▒▒
+  ▒▒▒▒▒▒██▒██▒▒▒▒▒▒▒▒▒▒██▒▒▒
+  ▒██████▒▒████████▒▒▒▒██▒▒▒
+ */
+	private void SetDefaultValue() {
+		ResetCameraFieldOfView();
+		Map = UnityEngine.GameObject.Find("_Map").GetComponent<Map>();
+		fpc = GetComponent<FirstPersonCamera>();
+	}
+}
